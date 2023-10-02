229e85d62a09ab47f9bf4c80f26e8d7dc447d67a
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-10-02 13:51:51 -0700

    feature added: logo added

---
+++ src/Shared/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className=' flex justify-center items-center mt-5'>
+            <img src="/logo.png" alt="" className=''/>
+        </div>
+    );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h2 className=" font-poppins">Hellow</h2>
             
         </div>
     );
